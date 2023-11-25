8aa82e1926d387d802746efe5c4157362910925a
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-25 17:15:04 +0700

    Update avail-auto.sh

--- Projects/Avail/avail-auto.sh
+++ Projects/Avail/avail-auto.sh
@@ -68,7 +68,7 @@ sleep 1
 # Download new entrypoint.sh
 echo -e "\e[1m\e[32m7. Download new entrypoint.sh... \e[0m" && sleep 1
 cd $HOME
-wget -q -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/Technology/entrypoint.sh
+curl -o entrypoint.sh https://raw.githubusercontent.com/vnbnode/binaries/main/Projects/Avail/entrypoint.sh
 chmod +x entrypoint.sh
 NAMES=`docker ps | egrep 'availj/avail' | awk '{print $10}'`
 
