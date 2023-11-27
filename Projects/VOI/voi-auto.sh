2759a7f762de19aa4f6a91a46113ebecfccefcb0
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-27 09:47:14 +0700

    Update voi-auto.sh

--- Projects/VOI/voi-auto.sh
+++ Projects/VOI/voi-auto.sh
@@ -25,7 +25,9 @@ sleep 1
 
 # Install Node Algorand
 echo -e "\e[1m\e[32m2. Install Node Algorand... \e[0m" && sleep 1
-sudo systemctl stop algorand && sudo systemctl disable algorand
+sudo apt update && sudo apt install -y algorand &&
+sudo systemctl stop algorand &&
+sudo systemctl disable algorand 
 sleep 1
 
 # Setup goal
@@ -35,18 +37,18 @@ sleep 1
 
 # Configure Node VOI
 echo -e "\e[1m\e[32m4. Configure Node VOI... \e[0m" && sleep 1
-sudo algocfg set -p DNSBootstrapID -v "<network>.voi.network" -d /var/lib/algorand/ &&\
-sudo algocfg set -p EnableCatchupFromArchiveServers -v true -d /var/lib/algorand/ &&\
-sudo chown algorand:algorand /var/lib/algorand/config.json &&\
-sudo chmod g+w /var/lib/algorand/config.json && \
-sudo curl -s -o /var/lib/algorand/genesis.json https://testnet-api.voi.nodly.io/genesis &&\
-sudo chown algorand:algorand /var/lib/algorand/genesis.json &&\
-sudo cp /lib/systemd/system/algorand.service /etc/systemd/system/voi.service &&\
+sudo algocfg set -p DNSBootstrapID -v "<network>.voi.network" -d /var/lib/algorand/ &&
+sudo algocfg set -p EnableCatchupFromArchiveServers -v true -d /var/lib/algorand/ &&
+sudo chown algorand:algorand /var/lib/algorand/config.json &&
+sudo chmod g+w /var/lib/algorand/config.json &&
+sudo curl -s -o /var/lib/algorand/genesis.json https://testnet-api.voi.nodly.io/genesis &&
+sudo chown algorand:algorand /var/lib/algorand/genesis.json &&
+sudo cp /lib/systemd/system/algorand.service /etc/systemd/system/voi.service &&
 sudo sed -i 's/Algorand daemon/Voi daemon/g' /etc/systemd/system/voi.service
 sleep 1
 
 # Start Node VOI
 echo -e "\e[1m\e[32m5. Start Node VOI... \e[0m" && sleep 1
-sudo systemctl start voi && sudo systemctl enable voi &&\
-goal node catchup $(curl -s https://testnet-api.voi.nodly.io/v2/status|jq -r '.["last-catchpoint"]') &&\
+sudo systemctl start voi && sudo systemctl enable voi &&
+goal node catchup $(curl -s https://testnet-api.voi.nodly.io/v2/status|jq -r '.["last-catchpoint"]') &&
 goal node status -w 1000
