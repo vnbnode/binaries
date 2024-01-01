a88261c30ed3b68c5de1c5b221d3b10245fd8b26
VNBnode <128967122+vnbnode@users.noreply.github.com>
2024-01-01 09:40:04 +0700

    Update auto-run.sh

--- Projects/Nulink/auto-run.sh
+++ Projects/Nulink/auto-run.sh
@@ -1,1 +1,71 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
 
+# Logo
+sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/binaries/main/Logo/logo.sh | bash && sleep 1
+
+# Generate Geth Account
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.23-d901d853.tar.gz
+
+tar -xvzf geth-linux-amd64-1.10.23-d901d853.tar.gz
+
+cd geth-linux-amd64-1.10.23-d901d853/
+
+./geth account new --keystore ./keystore
+sleep 5
+echo -e "\e[1;32m \e[0m\e[1;36m${CYAN} SAVE YOUR ADDRESS AND PASSWORDS ${NC}\e[0m"
+echo -e "\e[1;32m \e[0m\e[1;36m${CYAN} SAVE YOUR PRIVATE KEY AND PATH ${NC}\e[0m"
+sleep 5
+
+# Install docker
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Pull the latest NuLink image
+
+docker pull nulink/nulink:latest
+cd /root
+mkdir nulink
+cp /root/geth-linux-amd64-1.10.23-d901d853/keystore/* /root/nulink
+chmod -R 777 /root/nulink
+
+apt install python3-pip
+pip install virtualenv
+virtualenv /root/nulink-venv
+source /root/nulink-venv/bin/activate
+wget https://download.nulink.org/release/core/nulink-0.5.0-py3-none-any.whl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+pip install nulink-0.5.0-py3-none-any.whl
+source /root/nulink-venv/bin/activate
+
+# Verify again
+source /root/nulink-venv/bin/activate
+python -c "import nulink"
+nulink --help
+
+# Initiate Worker
+read -r -p "NULINK_KEYSTORE_PASSWORD : " NULINK_KEYSTORE_PASSWORD
+sleep 0.5
+read -r -p "NULINK_OPERATOR_ETH_PASSWORD : " NULINK_OPERATOR_ETH_PASSWORD
+sleep 0.5
