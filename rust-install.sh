c0d87cd5c6f85f0a5a75ec123ba75f0576ae7ae7
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2024-01-01 15:50:57 +0700

    Update

---
+++ rust-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+
+# Logo
+sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/binaries/main/Logo/logo.sh | bash && sleep 1
+
+# Update
+cd $HOME
+echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
+sudo apt update && sudo apt upgrade -y
+sleep 1
+
+# Install
+sudo apt-get update
+sudo apt install build-essential
+sudo apt install --assume-yes git clang curl libssl-dev protobuf-compiler
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env
+rustup default stable
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
+rm $HOME/rust-install.sh
+
+echo -e "\e[1m\e[32mFINISH \e[0m" && sleep 1
