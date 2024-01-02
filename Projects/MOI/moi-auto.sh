cd1ff4ad2644a223b893bd9cff97f447fe04fd35
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2024-01-02 22:12:46 +0700

    Update

--- Projects/MOI/moi-auto.sh
+++ Projects/MOI/moi-auto.sh
@@ -22,22 +22,18 @@ echo -e "\e[1m\e[32m2. Installing package... \e[0m" && sleep 1
 sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
 sleep 1
 
-# Install Docker
-echo -e "\e[1m\e[32m3. Installing docker... \e[0m" && sleep 1
-sudo apt-get update
-sudo apt-get install \
-ca-certificates \
-curl \
-gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Check if Docker is already installed
+if command -v docker > /dev/null 2>&1; then
+echo "Docker is already installed."
+else
+# Docker is not installed, proceed with installation
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+rm $HOME/get-docker.sh
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker -v
+fi
 sleep 1
 
 # Fill data
