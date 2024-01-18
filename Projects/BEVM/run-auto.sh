116d837da1a15547f908fbe38497d1232b4f1084
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2024-01-18 23:56:34 +0700

    Update run-auto.sh

--- Projects/BEVM/run-auto.sh
+++ Projects/BEVM/run-auto.sh
@@ -57,7 +57,7 @@ sleep 1
 echo -e "\e[1m\e[32m5. Run node... \e[0m" && sleep 1
 sudo docker run -d -v /var/lib/node_bevm_test_storage:/root/.local/share/bevm btclayer2/bevm:v0.1.1 bevm "--chain=testnet" "--name=$Walletbevm" "--pruning=archive" --telemetry-url "wss://telemetry.bevm.io/submit 0"
 # NAMES=`docker ps | egrep 'bevm/bevm' | awk '{print $10}'`
-rm -r $HOME/run-auto.sh
+rm -r $HOME/bevm-auto.sh
 
 # Command check
 echo '====================== SETUP FINISHED ======================'
