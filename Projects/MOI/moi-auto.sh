e4f0a499cef5ee8f401e5f9a32dd8477fa7c19f4
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2024-01-19 00:54:07 +0700

    Update moi-auto.sh

--- Projects/MOI/moi-auto.sh
+++ Projects/MOI/moi-auto.sh
@@ -10,23 +10,30 @@ else
 fi
 
 # Logo
-sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/binaries/main/Logo/logo.sh | bash && sleep 1
+sleep 0.5 && curl -s https://raw.githubusercontent.com/vnbnode/binaries/main/Logo/logo.sh | bash && sleep 0.5
 
+# Choice Option
+PS3='Please enter your choice: '
+options=("Install Node" "Update Node" "Remove Node" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Install Node")
 # Update
 echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
 sudo apt update && sudo apt upgrade -y
-sleep 1
 
 # Package
 echo -e "\e[1m\e[32m2. Installing package... \e[0m" && sleep 1
 sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
-sleep 1
 
 # Check if Docker is already installed
+echo -e "\e[1m\e[32m3. Installing Docker... \e[0m" && sleep 1
 if command -v docker > /dev/null 2>&1; then
 echo "Docker is already installed."
 else
 # Docker is not installed, proceed with installation
+echo -e "\e[1m\e[32m3. Installing Docker... \e[0m" && sleep 1
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 rm $HOME/get-docker.sh
@@ -34,7 +41,6 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 docker -v
 fi
-sleep 1
 
 # Fill data
 echo -e "\e[1m\e[32m4. Fill data... \e[0m" && sleep 1
@@ -88,17 +94,14 @@ else
 	sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:v0.5.0-port server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
         docker update --restart=unless-stopped $container_name_moi
 fi
-sleep 1
 
 # Allow port 30333
 echo -e "\e[1m\e[32m6. Allow Port 1600 and 6000... \e[0m" && sleep 1
 sudo ufw allow 1600/tcp
 sudo ufw allow 6000/tcp
 sudo ufw allow 6000/udp
-sleep 1
 
 # NAMES=`docker ps | egrep 'sarvalabs/moipod' | awk '{print $18}'`
-cd $HOME
 rm $HOME/moi-auto.sh
 
 # Command check
@@ -110,4 +113,49 @@ echo -e "\e[1;32mStart your node: \e[0m\e[1;36msudo docker start $container_name
 echo -e "\e[1;32mRestart your node: \e[0m\e[1;36msudo docker restart $container_name_moi\e[0m"
 echo -e "\e[1;32mStop your node: \e[0m\e[1;36msudo docker stop $container_name_moi\e[0m"
 echo -e "\e[1;32mRemove: \e[0m\e[1;36msudo docker rm $container_name_moi\e[0m"
+echo '============================================================='            
+            break
+            ;;
+        "Update Node")
+# Update the Guardian Node
+echo -e "\e[1m\e[32mUpdate the Guardian Node... \e[0m" && sleep 1
+SelectVersion="Please choose: \n 1. CPU from 2015 or later\n 2. CPU from 2015 or earlier"
+echo -e "${SelectVersion}"
+read -p "Enter index: " version;
+if [ "$version" != "2" ];then
+    docker stop $container_name_moi
+    docker rm $container_name_moi
+    sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:latest server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
+    docker update --restart=unless-stopped $container_name_moi
+else
+    docker stop $container_name_moi
+    docker rm $container_name_moi
+	sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:v0.5.0-port server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
+    docker update --restart=unless-stopped $container_name_moi
+fi
+
+# Command check
+echo '====================== SETUP FINISHED ======================'
+echo -e "\e[1;32mView the logs from the running: \e[0m\e[1;36mtail -f $moi_dirpath/log/3*\e[0m"
+echo -e "\e[1;32mView the logs from the running: \e[0m\e[1;36msudo docker logs -f $container_name_moi\e[0m"
+echo -e "\e[1;32mCheck the list of containers: \e[0m\e[1;36msudo docker ps -a\e[0m"
+echo -e "\e[1;32mStart your node: \e[0m\e[1;36msudo docker start $container_name_moi\e[0m"
+echo -e "\e[1;32mRestart your node: \e[0m\e[1;36msudo docker restart $container_name_moi\e[0m"
+echo -e "\e[1;32mStop your node: \e[0m\e[1;36msudo docker stop $container_name_moi\e[0m"
+echo -e "\e[1;32mRemove: \e[0m\e[1;36msudo docker rm $container_name_moi\e[0m"
 echo '============================================================='
+            break
+            ;;
+        "Remove Node")
+# Remove the Guardian Node
+docker stop $container_name_moi
+docker rm $container_name_moi
+rm -r $HOME/moi
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
