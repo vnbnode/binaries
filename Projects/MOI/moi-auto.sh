709bc99f641e16dc1155fd549c8cc823a9f77a8e
MoneyLow <76662222+pnpn1212@users.noreply.github.com>
2024-03-14 02:08:42 +0700

    Update moi-auto.sh

--- Projects/MOI/moi-auto.sh
+++ Projects/MOI/moi-auto.sh
@@ -107,12 +107,12 @@ SelectVersion="Please choose: \n 1. CPU from 2015 or later\n 2. CPU from 2015 or
 echo -e "${SelectVersion}"
 read -p "Enter index: " version;
 if [ "$version" != "2" ];then
-	sudo docker run --network host --rm -it -w /data -v $(pwd):/data sarvalabs/moipod:latest register --data-dir $moi_dirpath --mnemonic-keystore-path $moi_dirpath/keystore.json --mnemonic-keystore-password $moi_passwd --watchdog-url https://babylon-watchdog.moi.technology/add --node-password $moi_passwd --network-rpc-url https://voyage-rpc.moi.technology/babylon --wallet-address $moi_address --node-index $moi_index --local-rpc-url http://$moi_ip:1600
-        sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:latest server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
+	sudo docker run -p 1600:1600 -p 6000:6000/tcp -p 6000:6000/udp --rm -it -w /data -v $(pwd):/data sarvalabs/moipod:latest register --data-dir $moi_dirpath --mnemonic-keystore-path $moi_dirpath/keystore.json --mnemonic-keystore-password $moi_passwd --watchdog-url https://babylon-watchdog.moi.technology/add --node-password $moi_passwd --network-rpc-url https://voyage-rpc.moi.technology/babylon --wallet-address $moi_address --node-index $moi_index --local-rpc-url http://$moi_ip:1600
+        sudo docker run --name $container_name_moi -p 1600:1600 -p 6000:6000/tcp -p 6000:6000/udp -it -d -w /data -v $(pwd):/data sarvalabs/moipod:latest server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
         docker update --restart=unless-stopped $container_name_moi
 else
-        sudo docker run --network host --rm -it -w /data -v $(pwd):/data sarvalabs/moipod:v0.6.4-port register --data-dir $moi_dirpath --mnemonic-keystore-path $moi_dirpath/keystore.json --mnemonic-keystore-password $moi_passwd --watchdog-url https://babylon-watchdog.moi.technology/add --node-password $moi_passwd --network-rpc-url https://voyage-rpc.moi.technology/babylon --wallet-address $moi_address --node-index $moi_index --local-rpc-url http://$moi_ip:1600
-	sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:v0.6.0-port server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
+        sudo docker run -p 1600:1600 -p 6000:6000/tcp -p 6000:6000/udp --rm -it -w /data -v $(pwd):/data sarvalabs/moipod:v0.6.4-port register --data-dir $moi_dirpath --mnemonic-keystore-path $moi_dirpath/keystore.json --mnemonic-keystore-password $moi_passwd --watchdog-url https://babylon-watchdog.moi.technology/add --node-password $moi_passwd --network-rpc-url https://voyage-rpc.moi.technology/babylon --wallet-address $moi_address --node-index $moi_index --local-rpc-url http://$moi_ip:1600
+	sudo docker run --name $container_name_moi -p 1600:1600 -p 6000:6000/tcp -p 6000:6000/udp -it -d -w /data -v $(pwd):/data sarvalabs/moipod:v0.6.0-port server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
         docker update --restart=unless-stopped $container_name_moi
 fi
 
@@ -146,14 +146,14 @@ if [ "$version" != "2" ];then
     docker rm $container_name_moi
     rm -r $HOME/moi/db
     rm -r $HOME/moi/consensus
-    sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:latest server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
+    sudo docker run --name $container_name_moi -p 1600:1600 -p 6000:6000/tcp -p 6000:6000/udp -it -d -w /data -v $(pwd):/data sarvalabs/moipod:latest server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
     docker update --restart=unless-stopped $container_name_moi
 else
     docker stop $container_name_moi
     docker rm $container_name_moi
     rm -r $HOME/moi/db
     rm -r $HOME/moi/consensus
-    sudo docker run --name $container_name_moi --network host -it -d -w /data -v $(pwd):/data sarvalabs/moipod:v0.6.4-port server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
+    sudo docker run --name $container_name_moi -p 1600:1600 -p 6000:6000/tcp -p 6000:6000/udp -it -d -w /data -v $(pwd):/data sarvalabs/moipod:v0.6.4-port server --babylon --data-dir $moi_dirpath --log-level DEBUG --node-password $moi_passwd
     docker update --restart=unless-stopped $container_name_moi
 fi
 
