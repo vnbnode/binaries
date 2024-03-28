95693d03b8b67c0842d4414f293c08ab5756cb9e
MoneyLow-VNBnode <76662222+pnpn1212@users.noreply.github.com>
2024-03-28 21:11:44 +0700

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -26,7 +26,7 @@ sleep 1
 # Go
 echo -e "\e[1m\e[32m3. Installing Go... \e[0m" && sleep 1
 ! [ -x "$(command -v go)" ] && {
-VER="1.20.3"
+VER="1.21.7"
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
@@ -34,6 +34,8 @@ rm "go$VER.linux-amd64.tar.gz"
 [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
 echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
 source $HOME/.bash_profile
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 }
 [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
