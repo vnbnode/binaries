81fa4bc1a136394ec6de85dfa2de2bccb30b194f
MoneyLow-VNBnode <76662222+pnpn1212@users.noreply.github.com>
2024-04-29 06:16:10 +0700

    Update update-binary.sh

--- update-binary.sh
+++ update-binary.sh
@@ -22,8 +22,17 @@ echo -e "\e[1m\e[32m2. Installing package... \e[0m" && sleep 1
 sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
 sleep 1
 
+# Check if Go is installed and remove it
+if dpkg -l | grep -qw docker; then
+echo -e "\e[1m\e[32m3. Removing old Go versions... \e[0m" && sleep 1
+sudo rm -rf /usr/local/go
+sudo rm /etc/paths.d/go
+sudo apt-get remove golang-go
+sudo apt-get remove --auto-remove golang-go
+fi
+
 # Go
-echo -e "\e[1m\e[32m3. Installing Go... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m4. Installing Go... \e[0m" && sleep 1
 VER="1.21.3"
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
